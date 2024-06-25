4d5a0ed5cebbef27dbec15155a15067c5dafe7d1
Muhammad Sulais <muh.sulais@gmail.com>
2024-06-25 18:59:50 +0700

    Add vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-    scopedStyleStrategy: 'class',
-    integrations: [solid()],
-    vite: {
-        css: {
-            modules: {
-                generateScopedName: '[hash:base64:8]',
-            }
-        }
+  scopedStyleStrategy: 'class',
+  integrations: [solid()],
+  vite: {
+    css: {
+      modules: {
+        generateScopedName: '[hash:base64:8]'
+      }
     }
+  },
+  output: "server",
+  adapter: vercel()
 });
