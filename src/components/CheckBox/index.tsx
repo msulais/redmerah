a2289a4af724c945fea586074ce30e6eb4de0b3f
Muhammad Sulais <muh.sulais@gmail.com>
2024-07-03 14:37:43 +0700

    added typecheck utils

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,47 @@
+import { Show, createEffect, createSignal, mergeProps, onMount, splitProps, type JSX, type ParentComponent } from "solid-js"
+
+import { _check, _children, _compact, _disabled, _onValueChanged, _type, _value } from "@/data/string"
+import { toggleAttribute } from "@/utils/attributes"
+
+import Icon from "@/components/Icon"
+import Button from "@/components/Button"
+import './index.scss'
+
+type CheckBoxProps = JSX.HTMLAttributes<HTMLDivElement> & {
+    value?: boolean
+    compact?: boolean
+    onValueChanged?: (value: boolean) => unknown
+    disabled?: boolean
+    type?: 'radio' | 'check'
+}
+
+const CheckBox: ParentComponent<CheckBoxProps> = ($props) => {
+    const $$props = mergeProps({ type: _check }, $props)
+    const [props, other] = splitProps($$props, [_type, _compact, _disabled, _children, _value, _onValueChanged])
+    const [isSelected, setIsSelected] = createSignal<boolean>(false)
+    let isInitialize: boolean = true
+
+    function changeValue(): void {
+        setIsSelected(v => !v)
+        if (props[_onValueChanged]) props[_onValueChanged](isSelected())
+    }
+
+    createEffect(() => {
+        if (!isInitialize && props[_value] && props[_value] != isSelected() && props[_onValueChanged]){
+            props[_onValueChanged](props[_value])
+        }
+        setIsSelected(v => props[_value] ?? v)
+        isInitialize = false
+    })
+
+    return (<div data-selected={toggleAttribute(isSelected())} data-disabled={toggleAttribute(props[_disabled])} class="checkbox" {...other} onClick={() => changeValue()}>
+        <Button compact={props[_compact]} disabled={props[_disabled]} iconOnly classList={{"checkbox-icon": true}}>
+            <Show when={props[_type] == _check} fallback={<Icon filled={isSelected()} code={0xED2F}/>}>
+                <Icon code={isSelected()? 0xE3CB : 0xE3D4}/>
+            </Show>
+        </Button>
+        {props[_children]}
+    </div>)
+}
+
+export default CheckBox
---
+++ src/utils/typecheck.ts
@@ -0,0 +1,33 @@
+import { _bigint, _boolean, _function, _isArray, _number, _object, _string, _symbol } from "@/data/string";
+
+export function isArray(arg: unknown): boolean {
+    return Array[_isArray](arg)
+}
+
+export function isNumber(arg: unknown): boolean {
+    return typeof arg == _number
+}
+
+export function isString(arg: unknown): boolean {
+    return typeof arg == _string
+}
+
+export function isBoolean(arg: unknown): boolean {
+    return typeof arg == _boolean
+}
+
+export function isFunction(arg: unknown): boolean {
+    return typeof arg == _function
+}
+
+export function isBigInt(arg: unknown): boolean {
+    return typeof arg == _bigint
+}
+
+export function isObject(arg: unknown): boolean {
+    return typeof arg == _object
+}
+
+export function isSymbol(arg: unknown): boolean {
+    return typeof arg == _symbol
+}
