2d7b5375761a0ebb57657f1038b19706ecdd3083
Muhammad Sulais <muh.sulais@gmail.com>
2024-07-22 18:31:43 +0700

    fix disable translate icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -17,6 +17,7 @@ const Icon: VoidComponent<IconProps> = ($props) => {
     return (<i 
         class='icon' 
         data-inline={toggleAttribute(props[_inline])} 
+        translate="no"
         {...other}>
         { String[_fromCharCode](props[_code] - (props[_filled]? 1 : 0)) }
     </i>)
