4fffc8d47c0e5210b39cbceed9a4d80eb50febed
Muhammad Sulais <muh.sulais@gmail.com>
2024-09-23 19:16:25 +0700

    add more constants

--- src/constants/math.ts
+++ src/constants/math.ts
@@ -4,3 +4,5 @@ export const getMath = Math
 export const getNumber = Number
 export const mathPI = getMath[_PI]
 export const mathE = getMath[_E]
+export const positiveInfinity = getNumber.POSITIVE_INFINITY
+export const negativeInfinity = getNumber.NEGATIVE_INFINITY
