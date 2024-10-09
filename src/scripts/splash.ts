c8f1b61f504aaad8f90d9c29a3cd161f08abd5cd
Muhammad Sulais <muh.sulais@gmail.com>
2024-10-09 11:58:56 +0700

    add remove splash screen script

---
+++ src/scripts/splash.ts
@@ -0,0 +1,18 @@
+import { _splash, _animate, _spring, _finished, _then, _remove } from "@/constants/string"
+import { AnimationEffectTiming } from "@/enums/animation"
+import { ElementIds } from "@/enums/ids"
+import { getElementById } from "@/utils/element"
+import { setTimeDelayed } from "@/utils/timeout"
+
+export function removeSplashScreen(): void {
+    setTimeDelayed(() => {
+        const splash_ref = getElementById(ElementIds[_splash])
+        splash_ref?.[_animate](
+            {opacity: 0},
+            {
+                duration: 1000,
+                easing: AnimationEffectTiming[_spring]
+            }
+        )[_finished][_then](() => splash_ref[_remove]())
+    })
+}
