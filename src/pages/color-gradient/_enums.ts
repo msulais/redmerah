ce4b2cdb11083bd53dd4665a3f0f86311efaa37e
Muhammad Sulais <muh.sulais@gmail.com>
2024-10-09 12:06:05 +0700

    finished /color-gradient

---
+++ src/pages/color-gradient/_enums.ts
@@ -0,0 +1,148 @@
+export enum GradientType {
+	linear = 'linear',
+	radial = 'radial',
+	conic = 'conic'
+}
+
+export enum RadialGradientShape {
+	ellipse = 'ellipse',
+	circle = 'circle'
+}
+
+/**
+ * This will crash if the browser not supported.
+ * Currenty not recommended to use in production.
+*/
+export enum RectangularColorSpace {
+	auto = 'auto',
+	srgb = 'srgb',
+	srgb_linear = 'srgb-linear',
+	display_p3 = 'display-p3',
+	a98_rgb = 'a98-rgb',
+	prophoto_rgb = 'prophoto-rgb',
+	rec2020 = 'rec2020',
+	lab = 'lab',
+	oklab = 'oklab',
+	xyz = 'xyz',
+	xyz_d50 = 'xyz-d50',
+	xyz_d65 = 'xyz-d65',
+}
+
+export enum PolarColorSpace {
+	auto = 'auto',
+	hsl = 'hsl',
+	hwb = 'hwb',
+	lch = 'lch',
+	oklch = 'oklch'
+}
+
+export enum HueInterpolationMethod {
+	auto = 'auto',
+	shorter = 'shorter',
+	longer = 'longer',
+	increasing = 'increasing',
+	decreasing = 'decreasing'
+}
+
+export enum Commands {
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } colorStopIndex `number`
+	@param { number } length `number` from `0` to `100` */
+	change_colorStopLength = 'a',
+
+	/** @param { number } gradientIndex `number` */
+	toggle_gradient_repeat = 'b',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } angle `number` from `0` to `360` */
+	change_gradient_angle = 'c',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { RectangularColorSpace | PolarColorSpace } colorspace `RectangularColorSpace | PolarColorSpace`*/
+	change_colorInterpolationMethod = 'd',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { HueInterpolationMethod } method `HueInterpolationMethod` */
+	change_hueInterpolationMethod = 'e',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } colorStopIndex `number`
+	@param { HEXColor } color `HEXColor` */
+	change_colorStopColor = 'f',
+
+	/** @param { number } gradientIndex `number` */
+	add_colorStop = 'g',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } colorStopIndex `number` */
+	remove_colorStop = 'h',
+
+	add_gradient = 'i',
+
+	/** @param { number } gradientIndex `number` */
+	remove_gradient = 'j',
+
+	/** @param { ColorModel } model `ColorModel` */
+	change_settings_colorModel = 'k',
+
+	/** @param { number } value `number` */
+	change_settings_aspectRatio = 'l',
+
+	/** @param { number } value `number` */
+	change_settings_borderRadius = 'm',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { GradientType } type `GradientType` */
+	change_gradient_type = 'n',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { RadialGradientShape } shape `RadialGradientShape` */
+	change_radialGradient_shape = 'o',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } x `number` from `0` to `100` */
+	change_gradient_positionX = 'p',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } y `number` from `0` to `100` */
+	change_gradient_positionY = 'q',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } size `number` */
+	change_radialGradient_size = 'r',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } width `number` from `0` to `100` */
+	change_radialGradient_width = 's',
+
+	/**
+	@param { number } gradientIndex `number`
+	@param { number } height `number` from `0` to `100` */
+	change_radialGradient_height = 't',
+
+	save_gradient = 'u',
+
+	/** @param { number } index `number` */
+	view_gradient_data = 'v',
+
+	/** @param { number } index `number` */
+	delete_gradient_data = 'w',
+}
+
+export enum ColorModel {
+	hex = 'hex',
+	rgba = 'rgba',
+	hsla = 'hsla'
+}
