41a8cb40e4185271e7840e6740a15b569b775df5
Muhammad Sulais <muh.sulais@gmail.com>
2024-10-09 19:54:47 +0700

    add last page settings

--- src/pages/biru-ui/_App.tsx
+++ src/pages/biru-ui/_App.tsx
@@ -1,23 +1,39 @@
 import { createSignal, onMount, type VoidComponent } from "solid-js"
 
 import { Commands, Pages } from "./_enums"
-import { _animate, _button, _finished, _remove, _splash, _spring, _then } from "@/constants/string"
+import { _animate, _button, _createObjectStore, _finished, _forEach, _get, _includes, _key, _open, _put, _readObjectStore, _remove, _settings, _splash, _spring, _then, _value, _writeObjectStore } from "@/constants/string"
 import { AnimationEffectTiming } from "@/enums/animation"
 import { ElementIds } from "@/enums/ids"
 import { getElementById } from "@/utils/element"
 import { setMicrotask } from "@/utils/timeout"
+import { IDB } from "@/utils/indexeddb"
+import { type ObjectStoreSettings, ObjectStoreNames, ObjectStoreSettingsKeys } from "./_storage"
+import { DatabaseNames } from "@/enums/storage"
 
 import App from "@/components/App"
 import AppBar from './_AppBar'
 import SideNavigation from './_SideNavigation'
 import Body from './_Body'
 import CSS from './_styles.module.scss'
+import { ALL_PAGES_ENUM } from "./_constants"
 
 const _: VoidComponent = () => {
+	const db = new IDB(DatabaseNames.biruUi)
 	const [page, setPage] = createSignal<Pages>(Pages[_button])
+
+    function saveSettings(...items: [key: ObjectStoreSettingsKeys, value: unknown][]): void {
+        const store_settings = db[_writeObjectStore](ObjectStoreNames[_settings])
+		items[_forEach](item => store_settings?.[_put]({
+			key: item[0],
+			value: item[1]
+		}))
+    }
+
 	function command(type: Commands, ...args: unknown[]): unknown {
 		if (type == Commands.change_page) {
-			setPage(args[0] as Pages)
+			const page = args[0] as Pages
+			setPage(page)
+			saveSettings([ObjectStoreSettingsKeys.lastPage, page])
 		}
 		return
 	}
@@ -35,7 +51,35 @@ const _: VoidComponent = () => {
 		})
 	}
 
+	function initSettings(): void {
+		const store_settings = db[_readObjectStore](ObjectStoreNames[_settings])
+		if (store_settings == null) return
+
+		db
+		[_get]<ObjectStoreSettings<Pages>>(store_settings, ObjectStoreSettingsKeys.lastPage)
+		[_then](result => setPage(d => {
+			const page = result?.[_value] ?? d
+			return ALL_PAGES_ENUM[_includes](page)? page : d
+		}))
+	}
+
+	function initDatabase(): void {
+		db[_open]({
+			onSuccess() {
+				initSettings()
+			},
+			onUpgradeNeeded(_, db) {
+				db[_createObjectStore]<ObjectStoreSettings>({
+					name: ObjectStoreNames[_settings],
+					keyPath: _key,
+					indexs: [_key, _value]
+				})
+			},
+		})
+	}
+
 	onMount(() => {
+		initDatabase()
 		removeSplashScreen()
 	})
 
---
+++ src/pages/biru-ui/_storage.ts
@@ -0,0 +1,13 @@
+export type ObjectStoreSettings<T = unknown> = {
+	key: string
+	value: T
+}
+
+export enum ObjectStoreNames {
+	settings = 'settings',
+}
+
+export enum ObjectStoreSettingsKeys {
+	/** @param page `Pages` */
+	lastPage = 'lastPage',
+}
