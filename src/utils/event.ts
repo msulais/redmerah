375e6ceb210d9f2868618ed81fe8ce93f9c922f3
Muhammad Sulais <muh.sulais@gmail.com>
2024-12-28 00:36:00 +0700

    wrap every 'event.currentTarget' in function

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -45,20 +45,23 @@ export function event_stop_propagation(event: Event): void {
 	return event.stopPropagation()
 }
 
-export function event_current_target<T, U extends Event, V>(
-	event: U & { currentTarget: T; target: V }
-): T {
+export function event_current_target<T extends Event, U, V>(
+	event: T & {
+		currentTarget: U
+		target: V
+	}
+): U {
 	return event.currentTarget
 }
 
-export function event_call<T, E extends Event>(
-	event: E & { currentTarget: T; target: Element },
-	handler: JSX.EventHandlerUnion<T, E> | undefined,
+export function event_call<T extends Event, U>(
+	event: T & { currentTarget: U; target: Element },
+	handler: JSX.EventHandlerUnion<U, T> | undefined,
 ): boolean {
 	if (handler) {
-		if (is_function(handler)) (handler as JSX.EventHandler<T, E>)(event)
-		else (handler as JSX.BoundEventHandler<T, E>)[0](
-			(handler as JSX.BoundEventHandler<T, E>)[1],
+		if (is_function(handler)) (handler as JSX.EventHandler<U, T>)(event)
+		else (handler as JSX.BoundEventHandler<U, T>)[0](
+			(handler as JSX.BoundEventHandler<U, T>)[1],
 			event
 		)
 	}
