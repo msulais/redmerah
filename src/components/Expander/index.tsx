2671cdbcd64d4e636df0a6ce954b9b69f51bad7c
Muhammad Sulais <muh.sulais@gmail.com>
2025-02-05 01:07:42 +0700

    fix #64

--- src/components/Expander/index.tsx
+++ src/components/Expander/index.tsx
@@ -1,14 +1,14 @@
-import { createContext, createEffect, createSignal, mergeProps, onCleanup, onMount, Show, splitProps, useContext, type Accessor, type JSX, type ParentComponent } from "solid-js"
+import { createContext, createEffect, createSignal, mergeProps, Show, splitProps, useContext, type Accessor, type JSX, type ParentComponent } from "solid-js"
 import { mergeRefs } from "@solid-primitives/refs"
 
 import { attr_set_if_exist, classlist } from "@/utils/attributes"
-import { is_string } from "@/utils/typecheck"
-import { element_click, element_rect } from "@/utils/element"
-import { timeout_clear, timeout_set, wait } from "@/utils/timeout"
-import { event_call, event_current_target, event_prevent_default } from "@/utils/event"
-import { rect_height } from "@/utils/rect"
+import { element_animate, element_rect, element_style } from "@/utils/element"
+import { event_call, event_prevent_default } from "@/utils/event"
 import { promise_done } from "@/utils/object"
 import { ICON_CHEVRON_DOWN } from "@/constants/icons"
+import { rect_height } from "@/utils/rect"
+import { AnimationEffectTiming } from "@/enums/animation"
+import { array_join } from "@/utils/array"
 
 import { RawIconButton } from "@/components/Button"
 import { List, RawList, type ListProps, type RawListProps } from "@/components/List"
@@ -103,13 +103,12 @@ type ExpanderProps = JSX.DetailsHtmlAttributes<HTMLDetailsElement> & {
 }
 
 const Expander: ParentComponent<ExpanderProps> = ($props) => {
-	const BORDER_BOTTOM_WIDTH = 1
 	const [props, other] = splitProps(
 		mergeProps({c_variant: ExpanderVariant.tonal}, $props),
 		[
 			'children', 'class', 'c_attr_header',
 			'c_attr_body', 'c_header', 'c_variant',
-			'open', 'onToggle', 'ref'
+			'open'
 		]
 	)
 	const [header_props, header_props_other] = splitProps(
@@ -118,40 +117,11 @@ const Expander: ParentComponent<ExpanderProps> = ($props) => {
 	)
 	const [body_props, body_props_other] = splitProps(
 		props.c_attr_body! ?? {},
-		['class', 'style']
+		['class', 'ref']
 	)
 	const [is_open, set_is_open] = createSignal<boolean>(false)
-	const [content_height, set_content_height] = createSignal<number>(0)
-
-	// We expect no animation when the default state of <details> opened by default
-	const [is_mounted, set_is_mounted] = createSignal<boolean>(false)
 	let div_content_ref: HTMLDivElement
-	let expander_ref: HTMLDetailsElement
-
-	onMount(() => {
-		let t: number | null = null
-		const update = () => {
-			if (!div_content_ref) return
-			if (t != null) timeout_clear(t)
-			t = timeout_set(() => {
-				const height = rect_height(element_rect(div_content_ref)) + BORDER_BOTTOM_WIDTH
-				if (content_height() != height) set_content_height(height)
-				t = null
-			}, 50)
-		}
-		const resizeObserver = new ResizeObserver(update)
-		const mutationObserver = new MutationObserver(update)
-
-		set_content_height(rect_height(element_rect(div_content_ref)) + BORDER_BOTTOM_WIDTH)
-		set_is_mounted(true)
-		resizeObserver.observe(expander_ref!, { box: "border-box" })
-		mutationObserver.observe(expander_ref!, { subtree: true, childList: true })
-
-		onCleanup(() => {
-			resizeObserver.disconnect()
-			mutationObserver.disconnect()
-		})
-	})
+	let animation_done = true
 
 	createEffect(() => {
 		const open = props.open
@@ -159,14 +129,9 @@ const Expander: ParentComponent<ExpanderProps> = ($props) => {
 	})
 
 	return (<details
-		ref={mergeRefs(props.ref, r => expander_ref = r)}
 		class={classlist('c-expander', props.class)}
 		data-c-variant={props.c_variant}
-		onToggle={ev => {
-			event_call(ev, props.onToggle)
-			set_is_open(event_current_target(ev).open)
-		}}
-		open={props.open}
+		open={is_open()}
 		{...other}>
 		<ExpanderContext.Provider
 			value={{
@@ -176,13 +141,47 @@ const Expander: ParentComponent<ExpanderProps> = ($props) => {
 			<summary
 				class={classlist('c-expander-header', header_props.class)}
 				onClick={(ev) => {
-					if (event_call(ev, header_props.onClick)) return
-					if (!is_open()) return;
-
-					const el = event_current_target(ev)
+					event_call(ev, header_props.onClick)
 					event_prevent_default(ev)
-					set_is_open(false)
-					promise_done(wait(200), () => element_click(el))
+					if (!animation_done) return
+
+					const options: KeyframeAnimationOptions = {
+						duration: 300,
+						easing: AnimationEffectTiming.spring
+					}
+					const rect = element_rect(div_content_ref)
+					const padding_top = element_style(div_content_ref, 'padding-top')
+					const padding_bottom = element_style(div_content_ref, 'padding-bottom')
+					const padding_right = element_style(div_content_ref, 'padding-right')
+					const padding_left = element_style(div_content_ref, 'padding-left')
+					const padding = array_join(
+						[padding_top, padding_right, padding_bottom, padding_left], ' '
+					)
+					const padding2 = array_join(
+						['0px', padding_right, '0px', padding_left], ' '
+					)
+					animation_done = false
+					if (is_open()) return promise_done(
+						element_animate(div_content_ref, {
+							opacity: [1, 0],
+							height: [rect_height(rect) + 'px', '0px'],
+							padding: [padding, padding2]
+						}, options).finished,
+						() => {
+							animation_done = true
+							set_is_open(false)
+						}
+					)
+
+					set_is_open(true)
+					promise_done(
+						element_animate(div_content_ref, {
+							opacity: [0, 1],
+							height: ['0px', rect_height(rect) + 'px'],
+							padding: [padding2, padding]
+						}, options).finished,
+						() => animation_done = true
+					)
 				}}
 				{...header_props_other}>
 				{props.c_header}
@@ -191,19 +190,9 @@ const Expander: ParentComponent<ExpanderProps> = ($props) => {
 				class={classlist('c-expander-body', body_props.class)}
 				data-c-variant={props.c_variant}
 				data-c-open={attr_set_if_exist(is_open())}
-				style={is_string(body_props.style)
-					? body_props.style
-					: {
-						height: is_mounted()
-							? (is_open() ? content_height() : 0) + 'px'
-							: undefined,
-						...body_props.style as JSX.CSSProperties
-					}
-				}
+				ref={mergeRefs(body_props.ref, r => div_content_ref = r)}
 				{...body_props_other}>
-				<div ref={r => div_content_ref = r}>
-					{props.children}
-				</div>
+				{props.children}
 			</div>
 		</ExpanderContext.Provider>
 	</details>)
