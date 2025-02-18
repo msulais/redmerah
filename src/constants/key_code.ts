c9132afe84fbf0d3e6ae2362bcad8f5398654dd3
Muhammad Sulais <muh.sulais@gmail.com>
2025-02-18 10:07:34 +0700

    [constants:key_code] add new key code

--- src/constants/key_code.ts
+++ src/constants/key_code.ts
@@ -1,8 +1,27 @@
+// A
 export const KEY_ARROW_DOWN = 'ArrowDown'
 export const KEY_ARROW_LEFT = 'ArrowLeft'
 export const KEY_ARROW_RIGHT = 'ArrowRight'
 export const KEY_ARROW_UP = 'ArrowUp'
+
+// D
+export const KEY_DIGIT_0 = 'Digit0'
+export const KEY_DIGIT_1 = 'Digit1'
+export const KEY_DIGIT_2 = 'Digit2'
+export const KEY_DIGIT_3 = 'Digit3'
+export const KEY_DIGIT_4 = 'Digit4'
+export const KEY_DIGIT_5 = 'Digit5'
+export const KEY_DIGIT_6 = 'Digit6'
+export const KEY_DIGIT_7 = 'Digit7'
+export const KEY_DIGIT_8 = 'Digit8'
+export const KEY_DIGIT_9 = 'Digit9'
+
+// E
 export const KEY_ESCAPE = 'Escape'
-export const KEY_TAB = 'Tab'
-export const KEY_SPACE = 'Space'
 export const KEY_ENTER = 'Enter'
+
+// S
+export const KEY_SPACE = 'Space'
+
+// T
+export const KEY_TAB = 'Tab'
