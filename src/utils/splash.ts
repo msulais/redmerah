09cf8eee45ecf8b017a77f36469a916453e873be
Muhammad Sulais <muh.sulais@gmail.com>
2025-02-25 08:51:26 +0700

    forget to add file

---
+++ src/utils/splash.ts
@@ -0,0 +1,60 @@
+import { AnimationEffectTiming } from "@/enums/animation"
+import { BodyAttributes } from "@/enums/attributes"
+import { ElementIds } from "@/enums/ids"
+import { animationIsOn } from "@/utils/animation"
+import { attrGet, attrRemove } from "@/utils/attributes"
+import { documentBody } from "@/utils/document"
+import { elementRemove, elementById, elementStyleSet, elementStyleRemove, elementAnimate } from "@/utils/element"
+import { numberParse, numberSafe } from "@/utils/number"
+import { promiseDone } from "@/utils/object"
+import { timeTimerSet } from "@/utils/time"
+import { windowScrollTo } from "@/utils/window"
+
+let COMPONENT_COUNT: number = 0
+let COMPONENT_COUNT_MAX: number | null = null
+
+export function removeSplashScreen(timeout: number = 0): void {
+	timeTimerSet(() => {
+		const splashRef = elementById(ElementIds.splash)
+		if (!splashRef) return;
+
+		const scrollY = window.scrollY // original scroll offset Y
+		const animationOption = {duration: 500, easing: AnimationEffectTiming.spring}
+		const body = documentBody()
+		elementRemove(splashRef)
+		attrRemove(documentBody(), BodyAttributes.componentCount)
+
+		if (animationIsOn()) {
+			windowScrollTo({top: 0, behavior: 'instant'})
+			elementStyleSet(body, 'will-change', 'transform, opacity')
+			elementStyleSet(body, 'overflow', 'hidden')
+			promiseDone(
+				elementAnimate(body, {
+					transform: ['translateY(10vmin)', 'translateY(0)'],
+					opacity: [0, 1],
+				}, animationOption).finished,
+				() => {
+					elementStyleRemove(body, 'will-change')
+					elementStyleRemove(body, 'overflow')
+					windowScrollTo({top: scrollY, behavior: 'smooth'})
+				}
+			)
+		}
+
+	}, timeout)
+}
+
+export function tryRemoveSplashScreen(timeout: number = 0): void {
+	++COMPONENT_COUNT
+	if (COMPONENT_COUNT_MAX == null) COMPONENT_COUNT_MAX = numberSafe(
+		numberParse(attrGet(
+			documentBody(),
+			BodyAttributes.componentCount
+		) ?? '0'), 0
+	)
+
+	if (COMPONENT_COUNT >= COMPONENT_COUNT_MAX) {
+		removeSplashScreen(timeout)
+		attrRemove(documentBody(), BodyAttributes.componentCount)
+	}
+}
