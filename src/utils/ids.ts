78f4f9fdf81392167c598008b256ddd9a7bb071d
Muhammad Sulais <muh.sulais@gmail.com>
2025-04-15 19:56:39 +0700

    [utils:ids] add new element id generator

---
+++ src/utils/ids.ts
@@ -0,0 +1,12 @@
+let ID_INDEX = 0
+
+export function createId(prefix?: string, suffix?: string): string {
+	const generate = () => {
+		++ID_INDEX
+		return (prefix ?? 'gid-') + ID_INDEX.toString(36) + (suffix ?? '')
+	}
+	let id = generate()
+	while (document.getElementById(id)) id = generate()
+
+	return id
+}
