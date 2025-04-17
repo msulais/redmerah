60adb6dd96bde5e3111fbc1d6e28cf2594ecfc1c
Muhammad Sulais <muh.sulais@gmail.com>
2025-04-17 22:18:21 +0700

    [utils:ids] check for server-side

--- src/utils/ids.ts
+++ src/utils/ids.ts
@@ -6,7 +6,11 @@ export function createId(prefix?: string, suffix?: string): string {
 		return (prefix ?? 'gid-') + ID_INDEX.toString(36) + (suffix ?? '')
 	}
 	let id = generate()
-	while (document.getElementById(id)) id = generate()
+
+	// check if client-side
+	if (document) {
+		while (document.getElementById(id)) id = generate()
+	}
 
 	return id
 }
