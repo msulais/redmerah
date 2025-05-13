8f80d97a4de73f391066b38263ab13aec805c10a
Muhammad Sulais <muh.sulais@gmail.com>
2025-05-13 15:26:01 +0700

    [enums:keyboard] add keyboard values

---
+++ src/enums/keyboard.ts
@@ -0,0 +1,18 @@
+export enum KeyboardCode {
+	backspace = 'Backspace',
+	enter     = 'Enter',
+	delete    = 'Delete',
+	space     = 'Space',
+	keyR      = 'KeyR',
+	keyC      = 'KeyC',
+}
+
+export enum KeyboardValue {
+	backspace  = 'Backspace',
+	enter      = 'Enter',
+	delete     = 'Delete',
+	space      = ' ',
+	plus       = '+',
+	minus      = '-',
+	underscore = '_'
+}
