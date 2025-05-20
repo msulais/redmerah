e2b62de29883e498fbb92171f75e9ded1a6a171e
Muhammad Sulais <muh.sulais@gmail.com>
2025-05-20 09:56:42 +0700

    [enums:ids] new id for Popover API Warning

--- src/enums/ids.ts
+++ src/enums/ids.ts
@@ -1,6 +1,7 @@
 export enum GlobalElementIds {
 	colorAccent = 'g-color-accent',
 	splash = 'g-splash',
+	popoverWarningNotice = 'g-popover-warning-notice',
 	textTooltip = 'c-text-tooltip',
 	tooltipListener = 'c-tooltip-listener',
 	popoverListener = 'c-popover-listener',
