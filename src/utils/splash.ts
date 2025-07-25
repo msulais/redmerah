478b52f81e9276f9c2493b8fdadd6eeb729bfc64
Muhammad Sulais <muh.sulais@gmail.com>
2025-07-25 18:30:28 +0700

    reduce scale of splash image

--- src/utils/splash.ts
+++ src/utils/splash.ts
@@ -14,15 +14,17 @@ export function removeSplashScreen(timeout: number = 0): void {
 			return
 		}
 
-		const animationOption = {duration: 250, easing: AnimationEffectTiming.springInverse}
-		splashRef.animate({
-			scale: [1, .9],
-			borderRadius: ['0', '8px'],
-			opacity: [1, 0]
-		}, {...animationOption, delay: 250}).finished.then(() => splashRef.remove())
+		const options = {
+			duration: 250,
+			easing: AnimationEffectTiming.springInverse
+		}
+		splashRef.animate(
+			{opacity: 0},
+			{...options, delay: options.duration}
+		).finished.then(() => splashRef.remove())
 		imgRef?.animate({
-			scale: 0,
+			scale: .5,
 			opacity: 0
-		}, animationOption).finished.then(() => imgRef.remove())
+		}, options).finished.then(() => imgRef.remove())
 	}, timeout)
 }
