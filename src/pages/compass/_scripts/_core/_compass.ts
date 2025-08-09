126836dcb88187d50ad3152431bcb067c2bc4c75
Muhammad Sulais <muh.sulais@gmail.com>
2025-08-09 23:53:25 +0700

    [apps:compass] fix number round

--- src/pages/compass/_scripts/_core/_compass.ts
+++ src/pages/compass/_scripts/_core/_compass.ts
@@ -6,14 +6,15 @@ const _compassRef = $(ElementIds.bd_compass) as unknown as SVGGElement
 
 function _initEvents(): void {
 	window.addEventListener('deviceorientationabsolute', (ev) => {
-		const alpha = ev.alpha
+		let alpha = ev.alpha
 		if (alpha === null) {
 			// TODO: show error
 			return
 		}
 
 		requestAnimationFrame(() => {
-			_textRef.textContent = Math.round(360 - alpha) + '°'
+			alpha = Math.round(alpha!)
+			_textRef.textContent = 360 - alpha + '°'
 			_compassRef.setAttribute('transform', `rotate(${alpha},640,788)`)
 		})
 	})
