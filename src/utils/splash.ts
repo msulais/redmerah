8e4a34d4bf65a77ce40db55a0a2718abe0039b24
Muhammad Sulais <muh.sulais@gmail.com>
2025-09-12 01:09:52 +0700

    [layouts:Splash] make it faster

--- src/utils/splash.ts
+++ src/utils/splash.ts
@@ -1,6 +1,4 @@
-import { AnimationEasing } from "@/enums/animation"
 import { GlobalElementIds } from "@/enums/ids"
-import { isAnimationAllowed } from "@/utils/animation"
 
 export function removeSplashScreen(timeout: number = 0): void {
 	const splashRef = document.getElementById(GlobalElementIds.splash)
@@ -8,23 +6,7 @@ export function removeSplashScreen(timeout: number = 0): void {
 
 	const imgRef = splashRef.querySelector('svg')
 	setTimeout(() => {
-		if (!isAnimationAllowed()) {
-			imgRef?.remove()
-			splashRef.remove()
-			return
-		}
-
-		const options = {
-			duration: 250,
-			easing: AnimationEasing.springInverse
-		}
-		splashRef.animate(
-			{opacity: 0},
-			{...options, delay: options.duration}
-		).finished.then(() => splashRef.remove())
-		imgRef?.animate({
-			scale: .5,
-			opacity: 0
-		}, options).finished.then(() => imgRef.remove())
+		imgRef?.remove()
+		splashRef.remove()
 	}, timeout)
 }
