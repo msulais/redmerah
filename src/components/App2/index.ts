9f182fa6f86df6115d1d5f859ab1a97357949bc0
Muhammad Sulais <muh.sulais@gmail.com>
2025-09-14 11:49:11 +0700

    [components:App2|AppBar2] new component

---
+++ src/components/App2/index.ts
@@ -0,0 +1,157 @@
+type AppProps = astroHTML.JSX.HTMLAttributes & {
+	AppTagName         ?: string
+	AppAppBarAttr      ?: astroHTML.JSX.HTMLAttributes
+	AppBottomBarAttr   ?: astroHTML.JSX.HTMLAttributes
+	AppBodyAttr        ?: astroHTML.JSX.HTMLAttributes
+	AppContainerAttr   ?: astroHTML.JSX.HTMLAttributes
+	AppLeftSideBarAttr ?: astroHTML.JSX.HTMLAttributes
+	AppRightSideBarAttr?: astroHTML.JSX.HTMLAttributes
+}
+
+type AppElement<T extends HTMLElement> = T
+
+type AppUpdateOptions<T extends AppElement<HTMLElement>> = {
+	AppChildren    ?: (Node | string)[] | boolean
+	AppAppBar      ?: (Node | string)[] | boolean
+	AppBottomBar   ?: (Node | string)[] | boolean
+	AppLeftSideBar ?: (Node | string)[] | boolean
+	AppRightSideBar?: (Node | string)[] | boolean
+	AppRefs?: {
+		app         ?(ref: T             ): unknown
+		appBar      ?(ref: HTMLDivElement): unknown
+		container   ?(ref: HTMLDivElement): unknown
+		leftSideBar ?(ref: HTMLDivElement): unknown
+		body        ?(ref: HTMLDivElement): unknown
+		rightSideBar?(ref: HTMLDivElement): unknown
+		bottomBar   ?(ref: HTMLDivElement): unknown
+	}
+}
+
+enum AppClasses {
+	app          = 'c-app',
+	appBar       = app + '-appbar',
+	container    = app + '-container',
+	leftSideBar  = app + '-left-sidebar',
+	rightSideBar = app + '-right-sidebar',
+	bottomBar    = app + '-bottombar',
+	body         = app + '-body'
+}
+
+function createAppRef<T extends AppElement<HTMLElement>>(
+	options?: AppUpdateOptions<T> & {AppTagName?: keyof HTMLElementTagNameMap}
+): T {
+	const appRef = document.createElement(options?.AppTagName ?? 'div')
+	return updateAppRef(appRef, options) as T
+}
+
+function updateAppRef<T extends AppElement<HTMLElement>>(appRef: T, options?: AppUpdateOptions<T>): T {
+	const refs = options?.AppRefs
+	appRef.classList.add(AppClasses.app)
+
+	// appbar
+	const appBarOption = options?.AppAppBar
+	let appBarRef = appRef.querySelector(`.${AppClasses.appBar}`) as HTMLDivElement | null
+	if (appBarOption === false) {
+		appBarRef?.replaceChildren()
+	}
+	else if (appBarOption !== undefined && appBarOption !== true) {
+		if (!appBarRef) {
+			appBarRef = document.createElement('div')
+			appBarRef.classList.add(AppClasses.appBar)
+		}
+
+		appBarRef.replaceChildren(...appBarOption)
+	}
+
+	// container
+	let containerRef = appRef.querySelector(`.${AppClasses.container}`) as HTMLDivElement | null
+	if (!containerRef) {
+		containerRef = document.createElement('div')
+		containerRef.classList.add(AppClasses.container)
+	}
+
+	// container -> leftsidebar
+	const leftSideBarOption = options?.AppLeftSideBar
+	let leftSideBarRef = containerRef.querySelector(`.${AppClasses.leftSideBar}`) as HTMLDivElement | null
+	if (leftSideBarOption === false) {
+		leftSideBarRef?.replaceChildren()
+	}
+	else if (leftSideBarOption !== undefined && leftSideBarOption !== true) {
+		if (!leftSideBarRef) {
+			leftSideBarRef = document.createElement('div')
+			leftSideBarRef.classList.add(AppClasses.leftSideBar)
+		}
+
+		leftSideBarRef.replaceChildren(...leftSideBarOption)
+	}
+
+	// container -> body
+	let bodyRef = appRef.querySelector(`.${AppClasses.body}`) as HTMLDivElement | null
+	if (!bodyRef) {
+		bodyRef = document.createElement('div')
+		bodyRef.classList.add(AppClasses.body)
+	}
+
+	const childrenOption = options?.AppChildren
+	if (childrenOption === false) {
+		bodyRef.replaceChildren()
+	}
+	else if (childrenOption !== undefined && childrenOption !== true) {
+		bodyRef.replaceChildren(...childrenOption)
+	}
+
+	// container -> rightsidebar
+	const rightSideBarOption = options?.AppRightSideBar
+	let rightSideBarRef = appRef.querySelector(`.${AppClasses.rightSideBar}`) as HTMLDivElement | null
+	if (rightSideBarOption === false) {
+		rightSideBarRef?.replaceChildren()
+	}
+	else if (rightSideBarOption !== undefined && rightSideBarOption !== true) {
+		if (!rightSideBarRef) {
+			rightSideBarRef = document.createElement('div')
+			rightSideBarRef.classList.add(AppClasses.rightSideBar)
+		}
+
+		rightSideBarRef.replaceChildren(...rightSideBarOption)
+	}
+
+	// bottombar
+	const bottomBarOption = options?.AppBottomBar
+	let bottomBarRef = appRef.querySelector(`.${AppClasses.bottomBar}`) as HTMLDivElement | null
+	if (bottomBarOption === false) {
+		bottomBarRef?.replaceChildren()
+	}
+	else if (bottomBarOption !== undefined && bottomBarOption !== true) {
+		if (!bottomBarRef) {
+			bottomBarRef = document.createElement('div')
+			bottomBarRef.classList.add(AppClasses.bottomBar)
+		}
+
+		bottomBarRef.replaceChildren(...bottomBarOption)
+	}
+
+	containerRef.replaceChildren(...[appBarRef, bodyRef, bottomBarRef].filter(
+		v => typeof v === 'string' || v instanceof Node
+	))
+
+	appRef.replaceChildren(...[leftSideBarOption, containerRef, rightSideBarRef].filter(
+		v => typeof v === 'string' || v instanceof Node
+	))
+	refs?.app?.(appRef)
+	refs?.container?.(containerRef)
+	refs?.body?.(bodyRef)
+	if (appBarRef) refs?.appBar?.(appBarRef)
+	if (leftSideBarRef) refs?.leftSideBar?.(leftSideBarRef)
+	if (rightSideBarRef) refs?.rightSideBar?.(rightSideBarRef)
+	if (bottomBarRef) refs?.bottomBar?.(bottomBarRef)
+	return appRef
+}
+
+export {
+	type AppProps,
+	type AppUpdateOptions,
+	type AppElement,
+	AppClasses,
+	createAppRef,
+	updateAppRef
+}
---
+++ src/components/AppBar2/index.ts
@@ -0,0 +1,149 @@
+type AppBarProps = astroHTML.JSX.HTMLAttributes & {
+	AppBarTagName     ?: string
+	AppBarContentAttr ?: astroHTML.JSX.HTMLAttributes
+	AppBarLeadingAttr ?: astroHTML.JSX.HTMLAttributes
+	AppBarTrailingAttr?: astroHTML.JSX.HTMLAttributes
+	AppBarHeadingAttr ?: astroHTML.JSX.HTMLAttributes
+	AppBarFlexAttr    ?: astroHTML.JSX.HTMLAttributes
+}
+
+type AppBarElement<T extends HTMLElement> = T
+
+type AppBarUpdateOptions<T extends AppBarElement<HTMLElement>> = {
+	AppBarChildren?: (Node | string)[] | boolean
+	AppBarLeading ?: (Node | string)[] | boolean
+	AppBarHeadline?: (Node | string)[] | boolean
+	AppBarTrailing?: (Node | string)[] | boolean
+	AppBarRefs?: {
+		appBar  ?(ref: T                 ): unknown
+		flex    ?(ref: HTMLDivElement    ): unknown
+		leading ?(ref: HTMLDivElement    ): unknown
+		trailing?(ref: HTMLDivElement    ): unknown
+		content ?(ref: HTMLDivElement    ): unknown
+		headline?(ref: HTMLHeadingElement): unknown
+	}
+}
+
+enum AppBarClasses {
+	appbar   = 'c-appbar',
+	leading  = appbar + '-leading',
+	trailing = appbar + '-trailing',
+	content  = appbar + '-content',
+	headline = appbar + '-headline',
+	flex = appbar + '-flex'
+}
+
+function createAppBarRef<T extends AppBarElement<HTMLElement>>(
+	options?: AppBarUpdateOptions<T> & {AppBarTagName?: keyof HTMLElementTagNameMap}
+): T {
+	const appBarRef = document.createElement(options?.AppBarTagName ?? 'header')
+	return updateAppBarRef(appBarRef, options) as T
+}
+
+function updateAppBarRef<T extends AppBarElement<HTMLElement>>(
+	appBarRef: T,
+	options?: AppBarUpdateOptions<T>
+): T {
+	const refs = options?.AppBarRefs
+	appBarRef.classList.add(AppBarClasses.appbar)
+
+	// leading
+	const leadingOption = options?.AppBarLeading
+	let leadingRef = appBarRef.querySelector(`.${AppBarClasses.leading}`) as HTMLDivElement | null
+	if (leadingOption === false) {
+		leadingRef?.replaceChildren()
+	}
+	else if (leadingOption !== undefined && leadingOption !== true) {
+		if (!leadingRef) {
+			leadingRef = document.createElement('div')
+			leadingRef.classList.add(AppBarClasses.leading)
+		}
+
+		leadingRef.replaceChildren(...leadingOption)
+	}
+
+	// content
+	let contentRef = appBarRef.querySelector(`.${AppBarClasses.content}`) as HTMLDivElement | null
+	if (!contentRef) {
+		contentRef = document.createElement('div')
+		contentRef.classList.add(AppBarClasses.content)
+	}
+
+	// content -> headline
+	const headlineOption = options?.AppBarHeadline
+	let headlineRef = contentRef.querySelector(`.${AppBarClasses.headline}`) as HTMLHeadingElement | null
+	if (headlineOption === false) {
+		headlineRef?.replaceChildren()
+	}
+	else if (headlineOption !== undefined && headlineOption !== true) {
+		if (!headlineRef) {
+			headlineRef = document.createElement('h2')
+			headlineRef.classList.add(AppBarClasses.headline)
+		}
+
+		headlineRef.replaceChildren(...headlineOption)
+	}
+
+	// content -> children
+	const children: (Node | string)[] = []
+	for (const node of contentRef.childNodes) {
+		if (headlineRef && node === headlineRef) continue
+
+		children.push(node)
+	}
+
+	const childrenOption = options?.AppBarChildren
+	if (childrenOption === false) {
+		children.length = 0
+	}
+	else if (childrenOption !== undefined && childrenOption !== true) {
+		children.length = 0
+		children.push(...childrenOption)
+	}
+
+	contentRef.replaceChildren(...[headlineRef, ...children].filter(
+		v => typeof v === 'string' || v instanceof Node
+	))
+
+	// flex
+	let flexRef = appBarRef.querySelector(`.${AppBarClasses.flex}`) as HTMLDivElement | null
+	if (!flexRef) {
+		flexRef = document.createElement('div')
+		flexRef.classList.add(AppBarClasses.flex)
+	}
+
+	// trailing
+	const trailingOption = options?.AppBarTrailing
+	let trailingRef = appBarRef.querySelector(`.${AppBarClasses.trailing}`) as HTMLDivElement | null
+	if (trailingOption === false) {
+		trailingRef?.replaceChildren()
+	}
+	else if (trailingOption !== undefined && trailingOption !== true) {
+		if (!trailingRef) {
+			trailingRef = document.createElement('div')
+			trailingRef.classList.add(AppBarClasses.trailing)
+		}
+
+		trailingRef.replaceChildren(...trailingOption)
+	}
+
+	appBarRef.replaceChildren(...[leadingRef, contentRef, flexRef, trailingRef].filter(
+		v => typeof v === 'string' || v instanceof Node
+	))
+	refs?.appBar?.(appBarRef)
+	refs?.content?.(contentRef)
+	refs?.flex?.(flexRef)
+	if (leadingRef) refs?.leading?.(leadingRef)
+	if (headlineRef) refs?.headline?.(headlineRef)
+	if (trailingRef) refs?.trailing?.(trailingRef)
+	return appBarRef
+}
+
+export {
+	type AppBarProps,
+	type AppBarUpdateOptions,
+	type AppBarElement,
+	AppBarClasses,
+	createAppBarRef,
+	updateAppBarRef
+}
