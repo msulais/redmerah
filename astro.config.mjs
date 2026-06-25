fc8260947a217554b898130124f3f3bb0e2f86b7
Muhammad Sulais <sulaismuhammad@gmail.com>
2026-06-25 22:54:37 +0700

    try fix vercel error

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,14 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
 	scopedStyleStrategy: 'class',
-	vite: { css: { modules: { generateScopedName: '[hash:base64:8]' }}},
+	vite: {
+		css: { modules: { generateScopedName: '[hash:base64:8]' }},
+		resolve: {
+			alias: {
+				'@': '/src',
+			},
+		},
+	},
 	output: 'static',
 	adapter: vercel(),
 	server: { port: 3100 }
